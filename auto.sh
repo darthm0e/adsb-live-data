1b53d0cd84de76ea777198cbcbcb1ef85166e17f
darthm0e <supamoe25@gmail.com>
2019-01-13 20:48:56 +0100

    update

--- auto.sh
+++ auto.sh
@@ -2,5 +2,5 @@ rm rangeview.kmz
 zip -r rangeview.zip rangeview.kml
 mv rangeview.zip rangeview.kmz
 
-cd /home/pi/git/adsb-live-data && git commit -a -m "update"
+cd /home/pi/git/adsb-live-data && git add . && git commit -a -m "update"
 cd /home/pi/git/adsb-live-data && git push
