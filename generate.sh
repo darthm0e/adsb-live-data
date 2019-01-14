0a218716b25ad1b7d213c5eb1c0d2b16951e23a3
darthm0e <supamoe25@gmail.com>
2019-01-14 02:30:02 +0100

    update

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
+sudo /home/pi/git/dump1090.socket30003/heatmap.pl -override -output /usr/share/dump1090-mutability/html/ && /home/pi/git/dump1090.socket30003/rangeview.pl -override -output /home/pi/git/adsb-live-data
 
-/home/pi/git/dump1090.socket30003/heatmap.pl -override -output /usr/share/dump1090-mutability/html/ && /home/pi/git/dump1090.socket30003/rangeview.pl -override -output /home/pi/git/adsb-live-data
